bb982547c3aa201893bafb7b7b871635c2cd7b7c
Descy <d3nnis.s@web.de>
2025-08-02 15:33:16 +0200

    cnmae

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PoTENt/',
+  base: '/',
   plugins: [vue()
     , tailwindcss()],
 })
